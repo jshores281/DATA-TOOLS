ccf9a4804c15e9e71a522b4da7d65d7261911e63
jshores281 <52839097+jshores281@users.noreply.github.com>
2021-10-12 14:27:33 -0500

    initial commit

--- pycsv.py
+++ pycsv.py
@@ -2,16 +2,47 @@
 ############################################################
 # TASKS
 ############################################################
-# ~ load data into csv
-# ~ read data out of csv
-#
+# ~ load data into csv from ?ext
+# ~ read data out of csv to ?ext
+# ~ 
 #
 ############################################################
 
 
 
 
+
+
 import csv 
+import argparse
+
+
+# ask user what type of file will be loading (?ext to csv)
+
+
+
+
+def load_text():
+	pass
+
+
+def load_pdf():
+	pass
+
+
+def load_csv():
+	pass
+
+
+def load_excel():
+	pass
+
+
+def load_gsheets():
+	pass
+
+
+
 
 
 
