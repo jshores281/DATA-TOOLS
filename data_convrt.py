989ac8ed7f1297dcdcb3a28fdd60637ed8197b20
jshores281 <52839097+jshores281@users.noreply.github.com>
2021-10-12 21:38:33 -0500

    updated error handling on first cmdline argument

--- data_convrt.py
+++ data_convrt.py
@@ -9,18 +9,25 @@
 ############################################################
 
 import csv, sys, os
-import argparse, pathlib
+import argparse
 
 os.system("cls")
 # ask user what type of file will be loading (?ext to csv) 
 
-parser = argparse.ArgumentParser(description='Process some DATA.', usage='data_convrt.py [txt, pdf, csv, google sheets]')
+parser = argparse.ArgumentParser('help', 
+								description='Process some DATA.', 
+								usage='data_convrt.py [txt, pdf, csv, google sheets]')
 
-parser.add_argument('string', metavar='[STRING]', type=str, nargs='+',
-                    help='Specify which type of file reading [txt, pdf, csv, google sheets]')
+parser.add_argument('string', 
+					metavar='[STRING]', 
+					type=str, 
+					nargs='+', 
+					help='Specify which type of file reading.')
+parser.add_argument('read', 
+					type=argparse.FileType('r'), 
+					help='read command of specified file.')
 
-parser.add_argument('read', type=argparse.FileType('r'),
-            		help='read command ')
+#parser.add_argument('write', type=argparse.FileType('w'), help='write command of specified file.')
 
 #parser.add_argument( 'convert', default=sys.stdout, type=argparse.FileType('w'),help='the location where data will be written')
 
@@ -30,14 +37,14 @@ args = parser.parse_args()
 
 
 def load_text():
-	with args.read.open('r') as _READ:
+	with args.read as _READ:
 		print(_READ.read())
-
+		sys.exit()
 	
 
 
 def load_pdf():
-	pass
+	print("ooopss need a pdf")
 
 
 def load_csv():
@@ -52,16 +59,22 @@ def load_gsheets():
 	pass
 
 
-
+string_list = ["txt", "pdf", "csv", "gsheets"]
 
 for string in args.string:
 	if string == "txt":
 		load_text()
 	elif string == "pdf":
-		print("ooopss need a pdf")
+		load_pdf()
+	elif string == "csv":
+		load_csv()
+	
 
-
-
+for sl in string_list:
+	if string not in sl:
+		print("command not found")
+		parser.print_help()
+		break
 
 
 
